4e10fadbabfde417ce35e19ba91b43fee91927fd
Dragon Liu <49094455+Dragonliu2018@users.noreply.github.com>
2022-10-06 17:54:24 +0800

    Update index.js
    测试正确性

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ const exportFromNotion = async (destination, format) => {
       process.exit(1);
     }
 
-    console.log(`Exported ${task.status} pages.`);
+    console.log(`Exported ${task.status.pagesExported} pages.`);
 
     if (task.state === `success`) {
       exportURL = task.status.exportURL;
